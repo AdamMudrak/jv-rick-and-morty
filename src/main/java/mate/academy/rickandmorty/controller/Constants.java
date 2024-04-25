576ae277e9a2bbf149ec94c6126b93714bc663f9
AdamMudrak <mudrets2000@gmail.com>
2024-04-26 02:40:02 +0300

    Add Strings needed for swagger documentation

---
+++ src/main/java/mate/academy/rickandmorty/controller/Constants.java
@@ -0,0 +1,17 @@
+package mate.academy.rickandmorty.controller;
+
+public class Constants {
+    static final String RICK_AND_MORTY = "RICK_AND_MORTY API";
+    static final String RICK_AND_MORTY_DESCRIPTION =
+            "Here you'll find a comprehensive overview of functions of this app.";
+    static final String GET_RANDOM_CHARACTER_SUMMARY = "Gets a random character";
+    static final String GET_RANDOM_CHARACTER_DESCRIPTION =
+            "Generates a random ID and retrieves a "
+                    + "random character from a local DB by it";
+    static final String GET_CHARACTER_BY_NAME_PART_SUMMARY = "Gets a character by name part";
+    static final String GET_CHARACTER_BY_NAME_PART_DESCRIPTION = "Retrieves a list of characters "
+            + "whose name contain the sent string in any position";
+    static final String NAME_EXAMPLE = "Rick";
+    static final String SUCCESSFULLY_RETRIEVED = "Successfully retrieved";
+    static final String CODE_200 = "200";
+}
