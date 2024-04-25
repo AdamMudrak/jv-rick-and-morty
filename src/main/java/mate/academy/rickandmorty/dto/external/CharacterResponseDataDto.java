f5540962dbaff3a345a0a56f395d1e698cea7ef6
AdamMudrak <mudrets2000@gmail.com>
2024-04-25 21:36:13 +0300

    Change naming and data type int ResponseDTO

--- src/main/java/mate/academy/rickandmorty/dto/external/CharacterResponseDataDto.java
+++ src/main/java/mate/academy/rickandmorty/dto/external/CharacterResponseDataDto.java
@@ -1,8 +1,7 @@
 package mate.academy.rickandmorty.dto.external;
 
-import java.util.Map;
 import mate.academy.rickandmorty.dto.internal.CharacterDto;
 
 public record CharacterResponseDataDto(
-        CharacterMetaDataDto meta, Map<String, CharacterDto> characters) {
+        CharacterInfoDto info, CharacterDto[] results) {
 }
