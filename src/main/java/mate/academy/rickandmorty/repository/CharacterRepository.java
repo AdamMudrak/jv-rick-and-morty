05048dee355df2fdac99538cad4a4de90ff33834
AdamMudrak <mudrets2000@gmail.com>
2024-04-26 02:02:13 +0300

    Add needed methods in repo

--- src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
+++ src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
@@ -1,7 +1,16 @@
 package mate.academy.rickandmorty.repository;
 
+import java.util.List;
 import mate.academy.rickandmorty.entity.LocalCharacter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CharacterRepository extends JpaRepository<LocalCharacter, Long> {
+    @Query(value = "SELECT MAX(localcharacter.id) FROM LocalCharacter localcharacter")
+    Long getMaxId();
+
+    @Query(value = "SELECT localcharacter "
+            + "FROM LocalCharacter localcharacter "
+            + "WHERE localcharacter.name LIKE %:name%")
+    List<LocalCharacter> findAllWhereNameContains(String name);
 }
