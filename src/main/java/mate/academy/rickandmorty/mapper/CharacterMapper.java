ddf85ca929820b6e4a70a1ea84a63d6a3ff8c4bc
AdamMudrak <mudrets2000@gmail.com>
2024-04-26 02:15:24 +0300

    Delete not needed methods, use according DTOs

--- src/main/java/mate/academy/rickandmorty/mapper/CharacterMapper.java
+++ src/main/java/mate/academy/rickandmorty/mapper/CharacterMapper.java
@@ -1,18 +1,14 @@
 package mate.academy.rickandmorty.mapper;
 
-import java.util.List;
 import mate.academy.rickandmorty.config.MapperConfig;
-import mate.academy.rickandmorty.dto.internal.CharacterDto;
+import mate.academy.rickandmorty.dto.internal.RemoteToLocalDto;
+import mate.academy.rickandmorty.dto.internal.RestClientDto;
 import mate.academy.rickandmorty.entity.LocalCharacter;
 import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
 public interface CharacterMapper {
-    CharacterDto toCharacterDto(LocalCharacter localCharacter);
+    LocalCharacter toLocalCharacterDto(RemoteToLocalDto remoteToLocalDto);
 
-    LocalCharacter toCharacter(CharacterDto characterDto);
-
-    List<CharacterDto> toCharacterDtos(List<LocalCharacter> localCharacters);
-
-    List<LocalCharacter> toCharacters(List<CharacterDto> charactersDto);
+    RestClientDto toRestDto(LocalCharacter localCharacter);
 }
