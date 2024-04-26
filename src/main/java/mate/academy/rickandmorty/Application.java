270142190f3d4fafb840ed10457144a40a87aee9
AdamMudrak <mudrets2000@gmail.com>
2024-04-26 22:03:15 +0300

    Delete method which simply promises headache, use one service and its logic for updating local DB

--- src/main/java/mate/academy/rickandmorty/Application.java
+++ src/main/java/mate/academy/rickandmorty/Application.java
@@ -1,13 +1,7 @@
 package mate.academy.rickandmorty;
 
-import java.awt.Desktop;
-import java.net.URI;
-import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import mate.academy.rickandmorty.dto.internal.RemoteToLocalDto;
 import mate.academy.rickandmorty.service.CharacterRemoteClient;
-import mate.academy.rickandmorty.service.CharacterService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,14 +14,13 @@ public class Application {
     private static final String COLOUR_RESET = "\u001B[0m";
     private static final String WAIT_MESSAGE = COLOUR_RED + "Local DB is being updated from server."
             + " Please wait for it to finish..." + COLOUR_RESET;
+    private static final String URL_OF_API = "http://localhost:8080/swagger-ui/index.html";
     private static final String SUCCESS_MESSAGE = COLOUR_RED
             + "Local DB updated from server successfully!"
             + System.lineSeparator()
-            + "You can now use the app!" + COLOUR_RESET;
-    private static final String HEADLESS_PROPERTY = "java.awt.headless";
-    private static final String HEADLESS_PROPERTY_VALUE = "false";
-    private static final String URL_OF_API = "http://localhost:8080/swagger-ui/index.html";
-    private final CharacterService characterService;
+            + "You can now use the app!" + COLOUR_RESET
+            + System.lineSeparator()
+            + "The API is now available at: " + URL_OF_API;
     private final CharacterRemoteClient characterRemoteClient;
 
     public static void main(String[] args) {
@@ -37,32 +30,9 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner() {
         return (args) -> {
-            fetchDataFromServer();
-            //openApiInBrowser();
+            System.out.println(WAIT_MESSAGE);
+            characterRemoteClient.fillLocalDbWithCharacters();
+            System.out.println(SUCCESS_MESSAGE);
         };
-    }
-
-    private void fetchDataFromServer() {
-        System.out.println(WAIT_MESSAGE);
-        for (Map.Entry<Integer, List<RemoteToLocalDto>> entry
-                : characterRemoteClient.getCharactersWithPageKeyMap().entrySet()) {
-            characterService.saveAllLocally(entry.getValue());
-        }
-        System.out.println(SUCCESS_MESSAGE);
-    }
-
-    /**
-     * This code should be run only on local machine
-     * as build fails on GitHub because there is
-     * no browser to be opened
-     */
-    private void openApiInBrowser() {
-        System.setProperty(HEADLESS_PROPERTY, HEADLESS_PROPERTY_VALUE);
-        Desktop desktop = Desktop.getDesktop();
-        try {
-            desktop.browse(new URI(URL_OF_API));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
